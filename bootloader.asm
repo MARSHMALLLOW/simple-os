11abdad752bb2c6a04333a69f7b4635fa32c599a
voyula <40173603+voyula@users.noreply.github.com>
2018-09-30 05:30:37 +0300

    Update bootloader.asm

--- bootloader.asm
+++ bootloader.asm
@@ -1,8 +1,7 @@
 section     .text
-global      _start                              ;must be declared for linker (ld)
+    global      _start                              ;must be declared for linker (ld)
 
 _start:                                         ;tell linker entry point
-
     mov     edx,len                             ;message length
     mov     ecx,msg                             ;message to write
     mov     ebx,1                               ;file descriptor (stdout)
@@ -13,6 +12,5 @@ _start:                                         ;tell linker entry point
     int     0x80                                ;call kernel
 
 section     .data
-
-msg     db  'Hello, world!',0xa                 ;our dear string
-len     equ $ - msg                             ;length of our dear string
+    msg     db  'Hello, world!',0xa                 ;our dear string
+    len     equ $ - msg                             ;length of our dear string
