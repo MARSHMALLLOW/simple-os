7b2e43af9affefefeea8d32632e38bc6419f89e0
voyula <40173603+voyula@users.noreply.github.com>
2018-09-30 04:48:11 +0300

    Update and rename bootloader.s to bootloader.asm

--- bootloader.asm
+++ bootloader.asm
@@ -9,7 +9,5 @@ _start:                     ;tell linker entry point
 	mov	eax, 1	    ;system call number (sys_exit)
 	int	0x80        ;call kernel
 
-section	.data
-
 msg	db	'Hello, world!',0xa	;our dear string
 len	equ	$ - msg			;length of our dear string
